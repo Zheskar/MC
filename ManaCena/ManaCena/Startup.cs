6931ad3c9e36b36e32a24939630cf3d24d020a03
Zheskar <ruslan.voitehovich@gmail.com>
2017-06-06 10:54:45 +0300

    init

---
+++ ManaCena/ManaCena/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ManaCena.Startup))]
+namespace ManaCena
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
